c3d08f4d2b794525f593c874571e68d9ac437d2f
wikidbrit <pflemingonline@gmail.com>
2021-08-18 17:18:37 +0200

    working on styling for the landing page some more. Reformatted footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,16 +1,49 @@
-import React from 'react'
+import React from "react"
 
-import Line from '../components/line'
+import Line from "../components/line"
 
 export default function footer() {
-    return (
-        <footer>
-            <div className="footwrap percent">            
-                <a href="https://github.com/wikidbrit" target="_blank" rel="noreferrer">GitHub</a><p> </p><a href="https://www.instagram.com/pfwikid/" target="_blank" rel="noreferrer">Instagram</a><p> </p><a href="https://www.linkedin.com/in/pflemingonline/" target="_blank" rel="noreferrer">LinkedIn</a>
-            </div>
-            <p className="percent"></p>
-            <p className="percent">Made with Gatsby<br></br>Content managed by Contentful<br></br>Form powered by GetForm</p>
-            <p className="percent">©2021 Paul Fleming</p>
-        </footer>
-    )
+  return (
+    <footer>
+      <div className="footwrap percent">
+        <a href="https://github.com/wikidbrit" target="_blank" rel="noreferrer">
+          GitHub /
+        </a>
+        <p> </p>
+        <a
+          href="https://www.instagram.com/pfwikid/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram /
+        </a>
+        <p> </p>
+        <a
+          href="https://www.linkedin.com/in/pflemingonline/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          LinkedIn
+        </a>
+      </div>
+      <Line />
+      <p className="percent">©2021 Paul Fleming</p>
+      <p className="percent references">
+        Made with{" "}
+        <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer">
+          Gatsby
+        </a>
+        <br></br>
+        Content managed by{" "}
+        <a href="https://www.contentful.com/" target="_blank" rel="noreferrer">
+          Contentful
+        </a>
+        <br></br>
+        Form powered by{" "}
+        <a href="https://getform.io/" target="_blank" rel="noreferrer">
+          GetForm
+        </a>
+      </p>
+    </footer>
+  )
 }
--- src/components/headerGrid.js
+++ src/components/headerGrid.js
@@ -15,7 +15,7 @@ export default function headerGrid() {
             </div>
             <div className="buttonArea">
                 <p className="innerTag code">{'<button>'}</p>
-                <a href="#contact" className="contactButton">Contact</a>
+                <a href="#contact" className="contactButton">Contact ▼</a>
                 <p className="innerTag code">{'</button>'}</p>
             </div>
             <div className="LogoArea">
