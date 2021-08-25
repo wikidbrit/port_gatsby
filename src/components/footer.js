7dfb3d5d4a1f44003684326471c32aea0bf6bf81
wikidbrit <pflemingonline@gmail.com>
2021-08-25 13:06:35 +0200

    corrected form colors when filled in

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-import GitHubIcon from '../images/icons/github-01.png'
-import InstagramIcon from '../images/icons/instagram-01.png'
-import LinkedInIcon from '../images/icons/linkedin-01.png'
+import GitHubIcon from "../images/icons/github-01.png"
+import InstagramIcon from "../images/icons/instagram-01.png"
+import LinkedInIcon from "../images/icons/linkedin-01.png"
 
 export default function footer() {
   return (
@@ -13,40 +13,37 @@ export default function footer() {
           target="_blank"
           rel="noreferrer"
         >
-          <img alt="Link to Git hub Profile" src={GitHubIcon} style={{height:'30px', margin:'5px'}}></img>
+          <img
+            alt="Link to Git hub Profile"
+            src={GitHubIcon}
+            style={{ height: "30px", margin: "5px" }}
+          ></img>
         </a>
         <a
           href="https://www.instagram.com/pfwikid/"
           target="_blank"
           rel="noreferrer"
         >
-          <img alt="link to instagram account" src={InstagramIcon} style={{height:'30px', margin:'5px'}}></img>
+          <img
+            alt="link to instagram account"
+            src={InstagramIcon}
+            style={{ height: "30px", margin: "5px" }}
+          ></img>
         </a>
         <a
           href="https://www.linkedin.com/in/pflemingonline/"
           target="_blank"
           rel="noreferrer"
         >
-          <img alt="link to linked in account" src={LinkedInIcon} style={{height:'30px', margin:'5px'}}></img>
+          <img
+            alt="link to linked in account"
+            src={LinkedInIcon}
+            style={{ height: "30px", margin: "5px" }}
+          ></img>
         </a>
       </div>
       <p className="percent">© 2021 Paul Fleming</p>
       <p className="percent references">
-        {/* Made with{" "}
-        <a href="https://www.gatsbyjs.com/" target="_blank" rel="noreferrer">
-          Gatsby
-        </a> */}
-        <br></br>
-        {/* Content managed by{" "}
-        <a href="https://www.contentful.com/" target="_blank" rel="noreferrer">
-          Contentful
-        </a> */}
-        paul.fleming@hyperisland.se
-        {/* <br></br>
-        Form powered by{" "}
-        <a href="https://getform.io/" target="_blank" rel="noreferrer">
-          GetForm
-        </a> */}
       </p>
     </footer>
   )
