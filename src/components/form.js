2eb6f339c53e22c4b4c20ae920e1eb25f0a98337
wikidbrit <pflemingonline@gmail.com>
2021-08-25 10:46:21 +0200

    form field works!

--- src/components/form.js
+++ src/components/form.js
@@ -60,47 +60,6 @@ export default function form() {
         </p>
       </form>
 
-      {/* <form
-        className="formBox"
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-      >
-        <input type="hidden" name="contact" value="contact" />
-        <input
-          className="inputField nameField"
-          type="text"
-          placeholder="Name"
-          name="contact"
-          required
-        />
-        <input
-          className="inputField emailField"
-          type="Email"
-          placeholder="Email"
-          name="contact"
-          required
-        />
-        <input
-          className="inputField emailField"
-          type="Email"
-          placeholder="Email"
-          name="contact"
-          required
-        />
-        <textarea
-          className="inputField messageField"
-          type="text"
-          placeholder="Message"
-          name="contact"
-          required
-          minLength="20"
-        />
-        <button className="formButton" type="submit">
-          SEND
-        </button>
-      </form> */}
       <p className=" formTag  code">{"</form>"}</p>
     </div>
   )
